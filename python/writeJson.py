783254fd0bd678e82c38451fef76355fbd0253e3
Lareine Jeng <23950069+ljeng@users.noreply.github.com>
2023-12-01 17:28:06 -0500

    Implement functionality on submit

--- python/writeJson.py
+++ python/writeJson.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import csv
 import json
 import os
 
@@ -15,7 +16,7 @@ def jsonify(folders):
     geo_dict = defaultdict(lambda: defaultdict(int))
     sex_dict = defaultdict(lambda: defaultdict(int))
     birth_year_dict = defaultdict(lambda: defaultdict(int))
-    name_sum = defaultdict(float)
+    name_sum = defaultdict(int)
     geo_sum = defaultdict(int)
     sex_sum = defaultdict(int)
     birth_year_sum = defaultdict(int)
@@ -38,8 +39,10 @@ def jsonify(folders):
         (sex_dict, 'sex'),
         (birth_year_dict, 'birth_year')]:
         dump(obj, 'count', filename)
-    sum_values = sum(name_sum.values())
-    dump({k: v / sum_values for k, v in name_sum.items()}, 'sum', 'name')
+    sum_occurrences = sum(name_sum.values())
+    dump({name: occurrences / sum_occurrences for name, occurrences in name_sum.items()},
+        'sum',
+        'name')
     for obj, filename in [
         (geo_sum, 'geo'),
         (sex_sum, 'sex'),
@@ -57,17 +60,48 @@ def normalize(filename):
             output_dict[superkey][subkey] = subvalue / sum_supervalues / divisor_dict[subkey]
     dump(output_dict, 'normalized', filename)
 
-def sortValues(filename):
-    obj = defaultdict(lambda: defaultdict(int))
-    for superkey, supervalue in load('normalized', filename).items():
-        obj[superkey] = sorted(
-            [(subkey, subvalue) for subkey, subvalue in supervalue.items()],
-            key = lambda x: x[1],
-            reverse=True)
-    dump(obj, 'sorted', filename)
+def jsonifyRace(filename):
+    reader = csv.reader(open(filename, 'r'))
+    next(reader)
+    count_dict = defaultdict(lambda: defaultdict(float))
+    race_sum = defaultdict(float)
+    firstname_sum = defaultdict(float)
+    for line in reader:
+        firstname, obs, pcthispanic, pctwhite, pctblack, pctapi, pctaian, pct2prace = line
+        firstname = firstname[0] + firstname[1:].lower()
+        obs = int(obs)
+        for race, pct in [('Hispanic', pcthispanic),
+            ('White', pctwhite),
+            ('Black', pctblack),
+            ('Asian', pctapi),
+            ('Indigenous', pctaian),
+            ('Mixed', pct2prace)]:
+            obs_pct = obs * float(pct) / 100
+            count_dict[race][firstname] += obs_pct
+            race_sum[race] += obs_pct
+            firstname_sum[firstname] += obs_pct
+    dump(count_dict, 'count', 'race')
+    dump(race_sum, 'sum', 'race')
+    sum_obs = sum(race_sum.values())
+    dump({firstname: obs_pct / sum_obs for firstname, obs_pct in firstname_sum.items()},
+        'sum',
+        'name.race')
+
+def normalizeRace():
+    filename = 'race'
+    input_dict = load('count', filename)
+    divisor_dict = load('sum', 'name.' + filename)
+    sum_values = sum(divisor_dict.values())
+    output_dict = defaultdict(lambda: defaultdict(float))
+    for superkey, supervalue in input_dict.items():
+        sum_supervalues = sum(supervalue.values())
+        for subkey, subvalue in supervalue.items():
+            output_dict[superkey][subkey] = subvalue / sum_supervalues / divisor_dict[subkey]
+    dump(output_dict, 'normalized', filename)
 
 jsonify(['namesbystate', 'namesbyterritory'])
+jsonifyRace('../firstnames.Data.csv')
 filenames = ['geo', 'sex', 'birth_year']
 for filename in filenames:
     normalize(filename)
-    sortValues(filename)
+normalizeRace()
--- script.js
+++ script.js
@@ -1,4 +1,57 @@
-let geojson_url = "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json";
+const geojson_url = "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json";
+const state_abbreviation = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+}
+let state = null;
 
 d3.json(geojson_url).then(function (geojson) {
     let fill = "white";
@@ -49,6 +102,7 @@ d3.json(geojson_url).then(function (geojson) {
             .transition()
             .duration(clickDuration)
             .style("fill", clickedFill);
+        state = d.properties.name;
     }
 
     d3.select("#map").selectAll("path")
@@ -67,92 +121,101 @@ d3.json(geojson_url).then(function (geojson) {
         .on("click", click);
 });
 
+function submit() {
+    const rangeBirthYear = 123;
+    const k = 100;
 
-document.addEventListener("DOMContentLoaded", async function () {
-    window.sorted_sex = await d3.json("json/sorted/sex.json");
-    window.sorted_birthYear = await d3.json("json/sorted/birth_year.json");
-    window.sum_sex = await d3.json("json/sum/sex.json");
-    window.sum_birthYear = await d3.json("json/sum/birth_year.json");
-    window.count_sex = await d3.json("json/count/sex.json");
-    window.count_birthYear = await d3.json("json/count/birth_year.json");
-    window.normalized_sex = await d3.json("json/normalized/sex.json");
-    window.normalized_birthYear = await d3
-        .json("json/normalized/birth_year.json");
-});
-
-async function submit() {
-    const n = 100;
+    const file_selection = {};
     const maleChecked = document.getElementById("maleCheckbox").checked;
     const femaleChecked = document.getElementById("femaleCheckbox").checked;
-    let names_sex = [];
-    if (maleChecked && !femaleChecked)
-        names_sex = getNames(window.sorted_sex, "male", n);
-    else if (femaleChecked && !maleChecked)
-        names_sex = getNames(window.sorted_sex, "female", n);
-    const minBirthYear = parseInt(document
-        .getElementById("minBirthYear")
-        .value,
-        10);
-    const maxBirthYear = parseInt(document
-        .getElementById("maxBirthYear")
-        .value,
-        10);
-    let superset_birthYear = [];
-    for (let birthYear = minBirthYear; birthYear <= maxBirthYear; birthYear++)
-        superset_birthYear
-            .push(...getNames(window.sorted_birthYear, birthYear, n));
-    const selected_birthYear = Array
-        .from({ length: maxBirthYear - minBirthYear + 1 },
-            (_, i) => minBirthYear + i);
-    const weight_birthYear = getWeight(selected_birthYear,
-        window.sum_birthYear);
-    const multiplier_birthYear = getMultiplier(superset_birthYear,
-        selected_birthYear,
-        weight_birthYear,
-        normalized_birthYear);
-    let names_birthYear = Object.entries(multiplier_birthYear);
-    names_birthYear.sort((x, y) => y[1] - x[1]);
-    const data = document.getElementById("data");
-    data.innerHTML = "";
-    let ul = document.createElement("ul");
-    names_sex.forEach(function (name) {
-      let li = document.createElement("li");
-      li.appendChild(document.createTextNode(name));
-      ul.appendChild(li);
-    });
-    names_birthYear.slice(0, n).forEach(function (entry) {
-      let li = document.createElement("li");
-      li.appendChild(document.createTextNode(entry[0]));
-      ul.appendChild(li);
-    });
-    data.appendChild(ul);
-}
-
-function getNames(sorted, key, n) {
-    return sorted[key].slice(0, n).map(entry => entry[0]);
-}
-
-function getWeight(selected, sum) {
-    const sum_weight = selected
-        .reduce((accumulator, selection) => accumulator + sum[selection], 0);
-    const weight = selected.reduce((accumulator, selection) => {
-        accumulator[selection] = sum[selection] / sum_weight;
-        return accumulator;
-    }, {});
-    return weight;
-}
-
-function getMultiplier(superset, selected, weight, normalized) {
-    const multiplier = {};
-    for (const name of superset) {
-        multiplier[name] = 0;
-        for (const selection of selected)
-            if (name in normalized[selection])
-                multiplier[name] += weight[selection] * normalized[selection][name];
+    let selection_sex = [];
+    if (maleChecked && !femaleChecked) {
+        selection_sex.push("male");
+        file_selection["sex"] = selection_sex;
     }
-    return multiplier;
-}
+    else if (femaleChecked && !maleChecked) {
+        selection_sex.push("female");
+        file_selection["sex"] = selection_sex;
+    }
+    let selection_birthYear = [];
+    for (let birthYear = parseInt(document.getElementById("minBirthYear").value, 10);
+        birthYear <= parseInt(document.getElementById("maxBirthYear").value, 10);
+        birthYear++
+    )
+        selection_birthYear.push(birthYear.toString());
+    if (selection_birthYear.length < rangeBirthYear)
+        file_selection["birth_year"] = selection_birthYear;
+    let selection_geo = [];
+    if (state != null) {
+        selection_geo.push(state_abbreviation[state]);
+        file_selection["geo"] = selection_geo;
+    }
+    let selection_race = [];
+    document.querySelectorAll('input[name="race"]:checked').forEach((checkbox) => {
+        selection_race.push(checkbox.value);
+    });
+    if (selection_race.length > 0) file_selection["race"] = selection_race;
+    let promises = [];
+    for (let file of Object.keys(file_selection)) {
+        promises.push(d3.json("json/sum/" + file + ".json"));
+        promises.push(d3.json("json/normalized/" + file + ".json"));
+    }
 
+    Promise.all(promises).then((_) => {
+        let sumMap = {};
+        let normalizedMap = {};
+
+        Object.keys(file_selection).forEach((file, i) => {
+            i *= 2;
+            sumMap[file] = _[i++];
+            normalizedMap[file] = _[i++];
+        });
+
+        let memo = {};
+
+        Object.entries(file_selection).forEach(([file, selection]) => {
+            memo[file] = {};
+            if (selection.length > 1) {
+                let n = 0;
+                for (let x of selection) n += sumMap[file][x];
+                const weightMap = Object.fromEntries(selection.map((x) => [x, sumMap[file][x] / n]));
+                for (let x of selection)
+                    for (let name of Object.keys(normalizedMap[file][x])) {
+                        memo[file][name] = 0;
+                        if (normalizedMap[file][x].hasOwnProperty(name))
+                            memo[file][name] += weightMap[x] * normalizedMap[file][x][name];
+                    }
+            }
+            else {
+                let x = selection[0];
+                for (let name of Object.keys(normalizedMap[file][x]))
+                    memo[file][name] = normalizedMap[file][x][name];
+            }
+        });
+
+        const name_multiplier = {};
+        for (let file of Object.keys(memo)) {
+            for (let name of Object.keys(memo[file])) {
+                if (!name_multiplier.hasOwnProperty(name))
+                    name_multiplier[name] = 1;
+                name_multiplier[name] *= memo[file][name];
+            }
+        }
+
+        const names_element = document.getElementById("names");
+        names_element.innerHTML = "";
+        let ul = document.createElement("ul");
+        Object.entries(name_multiplier)
+            .sort((a, b) => b[1] - a[1])
+            .slice(0, k)
+            .map(([name, multiplier]) => [name, multiplier]).forEach(function ([name, multiplier]) {
+            let li = document.createElement("li");
+            li.appendChild(document.createTextNode(`${name}: ${multiplier.toFixed(2)}`));
+            ul.appendChild(li);
+        });
+        names_element.appendChild(ul);
+    });
+}
 
 function selectAllRaces() {
     for (let checkbox of document.getElementsByName('race'))
@@ -164,46 +227,44 @@ function deselectAllRaces() {
         checkbox.checked = false;
 }
 
-function updateMaxMotherAgeOptions() {
-    let minMotherAge = document.getElementById('minMotherAge').value;
-    let maxMotherAge = document.getElementById('maxMotherAge');
-    maxMotherAge.querySelectorAll('option').forEach(function(option) {
-        option.disabled = false;
-    });
-    if (minMotherAge != 'under21') {
-        maxMotherAge.querySelector('[value="under21"]').disabled = true;
-        if (minMotherAge != 'over38') {
-            let minMotherAgeValue = parseInt(minMotherAge);
-            for (let i = 21; i <= 38; i++)
-                maxMotherAge
-                    .querySelector('[value="' + i + '"]')
-                    .disabled = (i < minMotherAgeValue);
-        }
-        else
-            for (let i = 21; i <= 38; i++)
-                maxMotherAge
-                    .querySelector('[value="' + i + '"]')
-                    .disabled = true;
-    }
-}
-
 function updateMinMotherAgeOptions() {
     let minMotherAge = document.getElementById('minMotherAge');
     let maxMotherAge = document.getElementById('maxMotherAge').value;
+
     minMotherAge.querySelectorAll('option').forEach(function(option) {
         option.disabled = false;
     });
+
     if (maxMotherAge != 'over38') {
         minMotherAge.querySelector('[value="over38"]').disabled = true;
         if (maxMotherAge != 'under21') {
             let maxMotherAgeValue = parseInt(maxMotherAge);
             for (let i = 21; i <= 38; i++)
-                minMotherAge.querySelector('[value="' + i + '"]')
-                    .disabled = (i > maxMotherAgeValue);
+                minMotherAge.querySelector('[value="' + i + '"]').disabled = (i > maxMotherAgeValue);
         }
         else
             for (let i = 21; i <= 38; i++)
-                minMotherAge.querySelector('[value="' + i + '"]')
-                    .disabled = true;
+                minMotherAge.querySelector('[value="' + i + '"]').disabled = true;
+    }
+}
+
+function updateMaxMotherAgeOptions() {
+    let minMotherAge = document.getElementById('minMotherAge').value;
+    let maxMotherAge = document.getElementById('maxMotherAge');
+
+    maxMotherAge.querySelectorAll('option').forEach(function(option) {
+        option.disabled = false;
+    });
+
+    if (minMotherAge != 'under21') {
+        maxMotherAge.querySelector('[value="under21"]').disabled = true;
+        if (minMotherAge != 'over38') {
+            let minMotherAgeValue = parseInt(minMotherAge);
+            for (let i = 21; i <= 38; i++)
+                maxMotherAge.querySelector('[value="' + i + '"]').disabled = (i < minMotherAgeValue);
+        }
+        else
+            for (let i = 21; i <= 38; i++)
+                maxMotherAge.querySelector('[value="' + i + '"]').disabled = true;
     }
 }
